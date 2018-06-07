eb4766797787c3c1cd241ad82ff7c7bfba9297d7
Dhruv Patel <mastervidja@gmail.com>
2018-06-07 17:57:41 +0530

    displayData function modified.

--- getBookList.php
+++ getBookList.php
@@ -1,28 +1,27 @@
 <?php
-
+global $query;
 $query = $_GET['query'];
 
 switch ($query) {
 
     case 'byclass':
-        
+
         if(!empty($_GET['class'])){
 
             connectDatabase();
-            
+
             $sql = "SELECT * FROM book_list WHERE class = '".$_GET['class']."'";
             //$sql = "SELECT DISTINCT subject FROM book_list WHERE class = '".$_GET['class']."'";
-            
+
             $result = $db->query($sql);
 
             displayData($result);
 
             $db->close();
-            
+
         }else{
             echo 'Content not found....';
         }
-        //fetching by class ends here.
     break;
 
     case 'search':
@@ -33,18 +32,18 @@ switch ($query) {
             $sql = "SELECT * FROM book_list WHERE subject LIKE '%".$_GET['searchString']."%' OR book_name LIKE '%".$_GET['searchString']."%'";
 
             $result = $db->query($sql);
-            
+
             displayData($result);
 
             $db->close();
-            
+
         }else{
             echo 'Content not found...';
         }
     break;
 
     default:
-        echo "From de-fault";
+        echo "echo from default";
     break;
 }
 
@@ -58,7 +57,8 @@ function connectDatabase(){
 
     //Create connection and select DB
     $db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
-    
+
+    //displaying error, if any
     if ($db->connect_error) {
     die("Unable to connect database: " . $db->connect_error);
     }
@@ -69,34 +69,37 @@ function displayData($result){
         while ($book = $result->fetch_assoc()) {
             $bookList[] = $book;
         }
+        //to get all the distinct subjects
         $subject = array_values(array_unique(array_map(function($elem){return $elem['subject'];}, $bookList)));
-        //echo json_encode($bookList);
+        
+        //made an array with key as subject i.e. $data = [subname => [array of books], subname2 => [array of books...]]
         foreach ($subject as $sub) {
-            foreach ($bookList as $key => $value) {
-                echo json_encode($value);
+            foreach ($bookList as $book) {
+                if($book['subject'] == $sub){
+                    $data[$sub][] = $book;
+                }
             }
         }
-        //echo json_encode($subject);
-    }
 
-
-
-    /*
-    if($result->num_rows > 0){
-        echo "<div class=\"row\">";
-        while($book = $result->fetch_assoc()){
-            if($book['flag'] == 1){ 
-                $book['subject'] = $book['subject']."(Compressed File)";
+        //sending data to modal
+        foreach ($data as $key => $value) {
+            echo "<div class=\"row\">";
+            echo "<div class=\"col-sm-6 col-md-4\" style=\"text-align:center;\">";
+            echo'<h4>'.$key.'</h4>';
+            foreach ($value as $book) {
+                if($book['flag'] == 1){
+                    $book['book_name'] = $book['book_name']."(Compressed File)";
+                }
+                /*if($query == 'search'){
+                    $book['book_name'] = $book['book_name'].'(class-'.$book['class'].')';
+                }    */         
+                echo '<p><a href="'.$book['link'].'">'.$book['book_name'].'</a></p>';
             }
-            echo "<div class=\"col-sm-6\" style=\"text-align:center;\">";
-            echo '<h4>'.$book['subject'].'</h4>';
-            echo '<a href="'.$book['link'].'">'.$book['book_name'].'</a><hr/>';
             echo "</div>";
         }
-        echo "</div";
     }else{
         echo 'Content not found...';
-    }*/
+    }
 }
 
 ?>
